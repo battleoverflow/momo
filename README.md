39da046a91414b7abdfa9e150cfa40f8846f81c9
Hifumi1337 <hifumi1337@protonmail.com>
2022-09-06 00:24:08 -0500

    v0.0.4
    - Added Momo's texture (+ set her position)
    - Reset the map position to better line up with Momo
    - Momo is now in the game! (+ animated)

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Momo's Quest
 Momo's Quest is a C++ game using raylib (inspired by Nene's Quest from New Game!)
 
-You can download an executable under GitHub's Releases
+Momo's Quest is a short story about an adventurer who wanders into the woods and encounters a horrifying BOSS battle! She'll have to battle this boss with all her might, but in the end, it's up to the player if she wins or loses. Good luck!
 
-### Resources
-All sprites and art provided by the following:
-- [GameArt2D](https://www.gameart2d.com/)
-- [Elthen](https://elthen.itch.io/)
+<!-- ### Resources
+All sprites and art were provided by the following:
+- [LuizMelo](https://luizmelo.itch.io/) - Sprites
+- [Elthen](https://elthen.itch.io/) - Sprites
+- [Hifumi1337](https://github.com/Hifumi1337) - Background/Map -->
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void gameLoop(bool debug_mode) {
     const int width{1280}; // Window width
     const int height{720}; // Window height
 
-    std::string momo_version = "0.0.2";
+    std::string momo_version = "0.0.4";
 
     // Initialize raylib window
     InitWindow(width, height, ("Momo's Quest | v%s", momo_version.c_str()));
@@ -21,9 +21,34 @@ void gameLoop(bool debug_mode) {
     // Key mapping for speed (k_speed)
     float k_speed{4.0};
 
+    // Load the main character
+    Texture2D momo_idle = LoadTexture("src/textures/player/Idle.png");
+    Texture2D momo_run = LoadTexture("src/textures/player/Run.png");
+    Texture2D momo = LoadTexture("src/textures/player/Idle.png");
+
+    float number_of_sprites{10.0f};
+
+    // Sets Momo's position on the map
+    Vector2 momo_pos {
+        (float) width / 2.0f - 4.0f * (0.5f * (float) momo.width / number_of_sprites),
+        (float) width / 2.0f - 4.0f * (0.5f * (float) momo.height)
+    };
+
+    // Direction schema:
+    // 1 => right
+    // -1 => left
+    float dir_location{1.0f};
+
+    // Animate Momo
+    float running_time{};
+    int cur_frame{};
+    const int max_frames{6};
+    const float update_time{1.0f / 12.0f};
+    float delta_time{};
+
     SetTargetFPS(60); // Sets the frames per second
 
-    Vector2 map_position{0.0, 0.0}; // Sets the map position
+    Vector2 map_position{-900.0, -2300.0}; // Sets the map position
 
     // Checks if the window is open/closed
     while (!WindowShouldClose()) {
@@ -68,9 +93,39 @@ void gameLoop(bool debug_mode) {
         if (Vector2Length(k_direction) != 0.0) {
             // Sets the new map position based on the direction being pressed and normalizes the vector
             map_position = Vector2Subtract(map_position, Vector2Scale(Vector2Normalize(k_direction), k_speed));
+            
+            // Changes direction of sprite (Momo)
+            k_direction.x < 0.0f ? dir_location = -1.0f : dir_location = 1.0f;
+
+            number_of_sprites = 8.0f;
+            momo = momo_run;
+        } else {
+            number_of_sprites = 10.0f;
+            momo = momo_idle;
         }
 
-        DrawTextureEx(map, map_position, 0.0, 4.0, WHITE); // Draw the map
+        // Map
+        DrawTextureEx(map, map_position, 0.0, 4.0, WHITE);
+
+        delta_time = GetFrameTime();
+
+        // Animation Frame
+        running_time += delta_time;
+
+        if (running_time >= update_time) {
+            cur_frame++; // Increments frames
+            running_time = 0.0f; // Resets running time (+ resets spritesheet)
+
+            if (cur_frame > max_frames) {
+                cur_frame = 0; // Resets frame
+            }
+        }
+
+        // Momo
+        Rectangle start{cur_frame * (float) momo.width / number_of_sprites, 0.0f, dir_location * (float) momo.width / number_of_sprites, (float) momo.height};
+        Rectangle finish{momo_pos.x, momo_pos.y, 4.0f * (float) momo.width / number_of_sprites, 4.0f * (float) momo.height};
+        
+        DrawTexturePro(momo, start, finish, Vector2{}, 0.0f, WHITE);
 
         EndDrawing();
     }
